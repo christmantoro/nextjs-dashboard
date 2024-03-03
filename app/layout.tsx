336420703deed4421cd386124a530aa0ebca76af
christmantoro@gmail.com <christmantoro>
2024-03-03 16:51:07 +0700

    Font Improvement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import { inter } from '@/app/ui/fonts/fonts';
 
 export default function RootLayout({
   children,
@@ -7,8 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={'${inter.className} antialiased'}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
--- app/ui/fonts/fonts.ts
+++ app/ui/fonts/fonts.ts
@@ -3,6 +3,7 @@ import { Inter, Lusitana } from 'next/font/google';
 export const lusitana = Lusitana({
     subsets: ['latin'],
     display: 'swap',
+    weight: '400'
 })
 
 export const inter = Inter({
