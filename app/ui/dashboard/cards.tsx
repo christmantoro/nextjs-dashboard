ef24ecbf8bfd675dfd95071a3787cc62e161139d
christmantoro@gmail.com <christmantoro>
2024-03-06 17:40:09 +0700

    Card Component

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,4 +1,10 @@
 import { lusitana } from '@/app/ui/fonts/fonts';
+import React from 'react';
+import { fetchCardData } from '@/app/lib/data';
+import { Invoice } from '@/app/lib/definitions';
+
+
+
 
 import {
   BanknotesIcon,
@@ -16,18 +22,13 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      { <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+      <Card title="Total Customers" value={numberOfCustomers} type="customers" />}
     </>
   );
 }
